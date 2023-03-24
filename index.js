bcfbd3ebf13c06acb2fcbd0267f29cea1bc3eba3
Keyur Bisetty <keyur@Keyurs-MacBook-Pro.local>
2023-03-24 02:08:02 -0400

    refactored code and added package

--- index.js
+++ index.js
@@ -1,64 +1,77 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
+const { create } = require('xmlbuilder2');
 const { Triangle, Circle, Square } = require('./lib/shapes');
-const SvgBuilder = require('svg-builder');
 
-const questions = [
-  {
-    type: 'input',
-    name: 'text',
-    message: 'Enter up to three characters:',
-    validate: (input) => (input.length <= 3 ? true : 'Please enter up to three characters.')
-  },
-  {
-    type: 'input',
-    name: 'textColor',
-    message: 'Enter the text color (keyword or hexadecimal):'
-  },
-  {
-    type: 'list',
-    name: 'shape',
-    message: 'Select a shape:',
-    choices: ['circle', 'triangle', 'square']
-  },
-  {
-    type: 'input',
-    name: 'shapeColor',
-    message: 'Enter the shape color (keyword or hexadecimal):'
-  }
-];
-
-inquirer.prompt(questions).then((answers) => {
-  const { text, textColor, shape, shapeColor } = answers;
-  let selectedShape;
-
-  switch (shape) {
-    case 'triangle':
-      selectedShape = new Triangle();
-      break;
-    case 'circle':
-      selectedShape = new Circle();
-      break;
-    case 'square':
-      selectedShape = new Square();
-      break;
-  }
-
-  selectedShape.setColor(shapeColor);
-
-  const svg = new SvgBuilder(300, 200)
-    .addElement(selectedShape.render())
-    .addText(text, {
-      x: 150,
-      y: 100,
-      fontSize: 48,
-      fill: textColor,
-      fontFamily: 'Arial',
-      textAnchor: 'middle',
-      dominantBaseline: 'central'
-    })
-    .toString();
-
-  fs.writeFileSync('logo.svg', svg);
-  console.log('Generated logo.svg');
-});
+const promptUser = async () => {
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'text',
+      message: 'Enter up to three characters:',
+      validate: (input) => (input.length <= 3 ? true : 'Input must be 3 characters or less.'),
+    },
+    {
+      type: 'input',
+      name: 'textColor',
+      message: 'Enter the text color (keyword or hexadecimal):',
+    },
+    {
+      type: 'list',
+      name: 'shape',
+      message: 'Select a shape:',
+      choices: ['triangle', 'circle', 'square'],
+    },
+    {
+      type: 'input',
+      name: 'shapeColor',
+      message: 'Enter the shape color (keyword or hexadecimal):',
+    },
+  ]);
+};
+(async () => {
+    const { text, textColor, shape, shapeColor } = await promptUser();
+  
+    let selectedShape;
+    switch (shape) {
+      case 'triangle':
+        selectedShape = new Triangle();
+        break;
+      case 'circle':
+        selectedShape = new Circle();
+        break;
+      case 'square':
+        selectedShape = new Square();
+        break;
+    }
+    selectedShape.setColor(shapeColor);
+  
+    const builder = create({ version: '1.0' });
+    const svg = builder
+      .ele('svg', {
+        xmlns: 'http://www.w3.org/2000/svg',
+        width: 300,
+        height: 200,
+      });
+  
+    const shapeElement = svg.ele(selectedShape.render().name);
+    Object.entries(selectedShape.render().attrs).forEach(([attrName, attrValue]) => {
+      shapeElement.att(attrName, attrValue);
+    });
+  
+    svg
+      .ele('text', {
+        x: 150,
+        y: 100,
+        'font-size': 48,
+        fill: textColor,
+        'font-family': 'Arial',
+        'text-anchor': 'middle',
+        'dominant-baseline': 'central',
+      })
+      .txt(text);
+  
+    const svgString = svg.end({ prettyPrint: true });
+    fs.writeFileSync('logo.svg', svgString);
+    console.log('Generated logo.svg');
+  })();
